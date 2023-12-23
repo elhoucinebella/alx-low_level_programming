c2cb3094717918e91bb2bbb8da2a212c9bc305f9
elhoucinebella <elhoucine.bellamine@um5r.ac.ma>
2023-12-23 04:13:22 +0100

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
- * main - a program that prints a line with puts
- *
+ * main - a program that prints a line with puts function
  * return: always 0 (success)
  */
 int main(void)
-
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
