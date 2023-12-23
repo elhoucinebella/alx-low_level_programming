6de40540babd8dad5658ea14f9b8529e609c4527
elhoucinebella <elhoucine.bellamine@um5r.ac.ma>
2023-12-23 03:51:58 +0100

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
-
 #include<stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * return: always 0 (success)
+ */
+
 int main(void)
+
 	{
 	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
