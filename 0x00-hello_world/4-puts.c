3db9e17a8472fe4c71098306e9580e56d84a1e0a
elhoucinebella <elhoucine.bellamine@um5r.ac.ma>
2023-12-23 03:44:33 +0100

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
-/*include<stio.h>
- *a program that prints a string
- */
+
 #include<stdio.h>
 int main(void)
-	{		
+	{
 	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
-}	
+}
