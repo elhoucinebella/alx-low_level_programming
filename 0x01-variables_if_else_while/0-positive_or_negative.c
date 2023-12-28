cf651a162240c2e7d722ccff7a419d9ec0b01854
elhoucinebella <elhoucine.bellamine@um5r.ac.ma>
2023-12-29 00:20:50 +0100

    negative or positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,15 @@
 #include<stdlib.h>
 #include<time.h>
 #include<stdio.h>
+
 /**
  * main - Entry point
  *
- * Description: if statements
+ * Description: if statement project
  *
  * Return 0 always (success)
  **/
+
 int main(void)
 
 {
@@ -17,18 +19,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-	{
-	printf("%d is positive", n);
-	}
-
+	printf("%d is positive\n", n);
 	else if (n == 0)
-	{
-	printf("%d is zero", n);
-	}
-
+	printf("%d is zero\n", n);
 	else
-	{
-	printf("%dis negative", n);
-	}
+	printf("%d is negative\n", n);
 	return (0);
 }
