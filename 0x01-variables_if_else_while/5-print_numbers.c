28e2bf848c91e1f2db93508d7c365b23e0e1b78c
elhoucinebella <elhoucine.bellamine@um5r.ac.ma>
2023-12-31 03:24:00 +0100

    digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description: a program that prints numbers
+ * Return: 0 always (uccess)
+ */
+int main(void)
+{
+	int Number = 0;
+
+	for (Number = 0; Number <= 10; Number++)
+	{
+	printf("%d", Number);
+	}
+	{
+	putchar('\n');
+	}
+
+return (0);
+}
