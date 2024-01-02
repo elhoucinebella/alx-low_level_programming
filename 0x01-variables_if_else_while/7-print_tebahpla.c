ca1ceec04347e40ec346e4a7fba7a8a679256983
elhoucinebella <elhoucine.bellamine@um5r.ac.ma>
2024-01-02 02:08:32 +0100

    alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - Entry point.
+ * Description: a program that prints alphabers in lowercase.
+ * Return: 0 always (success)
+ */
+int main(void)
+{
+	char alphabets = 'z';
+
+	for (alphabets = 'z'; alphabets >= 'a'; alphabets--)
+
+	{
+		putchar(alphabets);
+	}
+
+	{
+		putchar('n');
+	}
+	return (0);
+}
