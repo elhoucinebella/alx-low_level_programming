9ae0627594896b05f2ec327a851db4707ae9c6bf
elhoucinebella <elhoucine.bellamine@um5r.ac.ma>
2024-01-08 16:58:33 +0100

    changes

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i, j;
 
-		for (i = 0; i <= 9; ++i)
+		for (i = 0; i <= 8; ++i)
 
 		{
 			for (j = i + 1; j <= 9; j++)
@@ -22,11 +22,14 @@ int main(void)
 				{
 					putchar(',');
 					putchar(' ');
-					putchar('\n');
 				}
 
 			}
+
 		}
+
+	putchar('\n');
+
 	}
 
 return (0);
