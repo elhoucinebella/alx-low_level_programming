ac779d42dde6bce7bdd4d8586356362ee400236e
elhoucinebella <elhoucine.bellamine@um5r.ac.ma>
2024-01-08 16:46:34 +0100

    two comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *Description: a program that prints combinations.
+ * Return: 0 always(success)
+ */
+
+int main(void)
+{
+	int i, j;
+
+		for (i = 0; i <= 9; ++i)
+
+		{
+			for (j = 1; j <= 9; j++)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+			{
+
+					if (i != 8 || j != 9)
+				{
+					putchar(',');
+					putchar(' ');
+					putchar('\n');
+				}
+
+			}
+		}
+	}
+
+return (0);
+}
