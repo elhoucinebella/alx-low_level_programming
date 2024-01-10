3a0c3b67b3f89ac9ef6de0b9a8a7a5caff845421
elhoucinebella <elhoucine.bellamine@um5r.ac.ma>
2024-01-10 18:54:51 +0100

    printing putchar7

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,23 @@
-#include<stdio.h>
+#include "main.h"
+
 /**
  * main - Entry point
- * a program that prints putchar
+ *
+ *Descrpition:  a program that prints putchar
  * Return: 0 always (success)
  */
 int main(void)
-{
-	{
-		printf("_putchar\n");
-	}
 
-return (0);
-}
+	{
+		_putchar('_');
+		_putchar('p');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h');
+		_putchar('a');
+		_putchar('r');
+		_putchar('\n');
+
+		return (0);
+	}
