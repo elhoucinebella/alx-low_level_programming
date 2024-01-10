778da9b553466dba9be73c9fe32c889b089aacde
elhoucinebella <elhoucine.bellamine@um5r.ac.ma>
2024-01-10 15:48:09 +0100

    printf

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * a program that prints putchar
+ * Return: 0 always (success)
+ */
+int main(void)
+{
+	{
+		printf("_putchar\n");
+	}
+
+return (0);
+}
