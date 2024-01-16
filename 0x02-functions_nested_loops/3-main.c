362553e4b7ab3820e526962a1cd217c63d0b6589
elhoucinebella <elhoucine.bellamine@um5r.ac.ma>
2024-01-16 16:55:15 +0100

    lowercase

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int r;
 
-	r = _islower('H');
+	r = _islower('h');
 	_putchar(r + '0');
-	r = _islower('o');
+	r = _islower('O');
 	_putchar(r + '0');
 	r = _islower(108);
 	_putchar(r + '0');
