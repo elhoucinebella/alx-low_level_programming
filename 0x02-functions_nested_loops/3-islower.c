514404538eee04c9e7433a0ac64d749e65e3ce7c
elhoucinebella <elhoucine.bellamine@um5r.ac.ma>
2024-01-16 16:48:33 +0100

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * int_islower(int c) - checks for lowercase characters
+ * @c: parameter to be checked
+ *
+ * Return: 1 or 0
+ */
+int _islower(int c)
+
+{
+	if (c <= 'a' && c <= 'z')
+
+		return (1);
+
+	else
+		return (0);
+}
