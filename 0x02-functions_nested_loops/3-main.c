1c9ceebeeb12af9ea2c1668590dfd46b3e8f3711
elhoucinebella <elhoucine.bellamine@um5r.ac.ma>
2024-01-17 18:33:11 +0100

    islower

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -1,20 +1,22 @@
 #include "main.h"
 
 /**
- *  * main - check the code.
- *   *
- *    * Return: Always 0.
- *     */
+ *main - check the code.
+ *
+ *Return: Always 0.
+ **/
 int main(void)
 {
 	int r;
 
 	r = _islower('h');
-	_putchar(r + '0');	
+	_putchar(r + '0');
 	r = _islower('O');
-      	_putchar(r + '0');
+	_putchar(r + '0');
 	r = _islower('I');
 	_putchar(r + '0');
+
 	_putchar('\n');
-					    return (0);
+
+	return (0);
 }
