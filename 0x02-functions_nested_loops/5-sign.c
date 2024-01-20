64dff9a29f1d1a4135ae931c12e08583a9c19a9c
elhoucinebella <elhoucine.bellamine@um5r.ac.ma>
2024-01-20 15:26:38 +0100

    printing signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,21 +7,22 @@
  */
 int print_sign(int n)
 {
+
 	if (n > 0)
 	{
 		_putchar(53);
+		return (1);
 	}
-	return (1);
 
-	if (n == 0)
+	else if (n == 0)
 	{
 		_putchar(48);
+		return (0);
 	}
-	return (0);
 
-		else 
-		{
-			_putchar(95);
-		}
-			return (-1);
+	else
+	{
+		_putchar(95);
+		return (-1);
+	}
 }
