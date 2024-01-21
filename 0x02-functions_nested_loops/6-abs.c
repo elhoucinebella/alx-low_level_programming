2322c67ddfd8db5fce7fbfb53917fdb48e07e034
elhoucinebella <elhoucine.bellamine@um5r.ac.ma>
2024-01-21 19:50:06 +0100

    returning the absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of int
+ * @a: function prameter
+ * Return: -a or a;
+ */
+int _abs(int a)
+{
+	if (a < 0)
+		return (-a);
+	else
+		return (a);
+}
