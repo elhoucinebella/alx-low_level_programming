eeccc61ee1eae6ca9bdd1f3c7f6bd4083b97df89
elhoucinebella <elhoucine.bellamine@um5r.ac.ma>
2024-02-05 20:49:53 +0100

    leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -13,7 +13,7 @@ void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
 	{
-		if ((month != 0 && month  <= 12) && (day != 0  && day <= 31))
+		if ((month >= 1 &&  month  <= 12) && (day >= 1 || day <= 31))
 		{
 		day++;
 		}
