7667d2de5bbb32ba3389e06a860566eea6f6ec86
elhoucinebella <elhoucine.bellamine@um5r.ac.ma>
2024-02-05 03:26:48 +0100

    hot fix

---
+++ 0x03-debugging/3-main_b.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+ /**
+  * main
+  *Description: a program that prints day of the yeae, and how
+  *many days left in the year.
+  *
+  *Return: 0
+  */
+int main(void)
+{
+	int month;
+	int day;
+	int year;
+
+	month = 02;
+	day = 29;
+	year = 2000;
+
+	printf("Date: %2i/%2i/%4i\n", month, day, year);
+
+	day = convert_day(month, day);
+
+	print_remaining_days(month, day, year);
+
+	return (0);
+}
