89a59b61fb4f286ce48fb3996f514e847f72feb9
elhoucinebella <elhoucine.bellamine@um5r.ac.ma>
2024-02-20 04:05:42 +0100

    prints numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers ranging from 0 to 9
+ *
+ * Return: no return
+ */
+
+void print_numbers(void)
+
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
