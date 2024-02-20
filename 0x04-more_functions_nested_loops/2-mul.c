8353c47078714cce11c75d7aee288995bee47acb
elhoucinebella <elhoucine.bellamine@um5r.ac.ma>
2024-02-20 01:59:04 +0100

    multiplies integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies integers
+ * @a: first integer
+ * @b: second integer
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
