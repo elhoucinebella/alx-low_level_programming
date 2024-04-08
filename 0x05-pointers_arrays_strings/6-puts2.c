233a95ddc84412b7bff8f02a6d45c67c6506149a
elhoucinebella <elhoucine.bellamine@um5r.ac.ma>
2024-04-08 22:41:40 +0000

    puts function

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 /**
+ * puts2 -  prints every other character of a string
  *
+ * @str: the string to be printed
  *
- *
- *
- *
+ *Return:
  */
 void puts2(char *str)
 {
@@ -13,10 +14,9 @@ void puts2(char *str)
 	if (str == NULL)
 		return;
 
-	while (str[index] != '\0')
+	while (str[index] != '\0' && str[index + 1] != '\0')
 	{
 		putchar(str[index]);
-		
 		index += 2;
 	}
 	putchar('\n');
